eb258e5804ecbe25f51c26bb4d0afb65353bc0cd
ngockhanh99 <>
2019-11-19 08:43:28 +0700

    phần đầu và phần giữa trang wep
    Nộp phần đầu (main) vào phần cuối của trang wep dưới dạng php

---
+++ BTL_CNW_BuiNgocKhanh_LeQuangHuy/topIndex.php
@@ -0,0 +1,49 @@
+
+
+
+
+<div class = "sub-top">
+</div>
+<div class="top">
+
+        <img id="logo" src="images/logo_dhtl.jpg">
+        <img id="covi" src="images/covn.jpg">
+        <img id="cous" src="images/comy.jpg"> 
+        <input type="text" name="" placeholder="Tìm kiếm thông tin ">
+        <button type="button">Tìm </button>       
+        <a id ="adv" href="login.php" style="font-size: 16px;">(+)</a> 
+        <a id="Login" href="login.php" style="font-size: 16px;">Login</a> 
+        <a id="Register" href="login.php" style="font-size: 16px;">Register</a>
+        
+</div> 
+    
+<div class ="box">
+    <div id = "menu">
+        <ul> 
+            <li><a href="#" title="Tin tức & thống báo" style="border-left: none">TIN TỨC & THÔNG BÁO </a></li>
+            <li>
+                <a href="#" title="Tuyển sinh">TUYỂN SINH </a>
+                <div class = "sub-menu">
+                <ul>
+                    <li><a href=""> Item1</a></li>
+                    <li><a href=""> Item1</a></li>
+                    <li><a href=""> Item1</a></li>
+                </ul>
+                    
+                </div>
+              
+            </li>
+            <li><a href="#" title="Liên hệ">LIÊN HỆ </a></li>
+            <li><a href="#" title="Giới thiệu">GIỚI THIỆU </a> </li>
+            <li><a href="#" title="Đào tạo">ĐÀO TẠO </a> </li>
+            <li><a href="#" title="Khoa học công nghệ">KHOA HỌC CÔNG NGHỆ </a>  </li>
+            <li><a href="#" title="Sinh viên">SINH VIÊN </a></li>
+            <li><a href="#" title="Đối ngoại">ĐỐI NGOẠI </a></li>
+        </ul>             
+    </div>
+      <img id="anh60nam" src="images/60nam.png">         
+</div>
+       
+          
+
+
