aba76473bc0e83da82886e7f8b8113e270d52b4a
lequanghuy999 <57338511+lequanghuy999@users.noreply.github.com>
2019-11-19 08:39:57 +0700

    Create footerIndex.php

---
+++ BTL_CNW_BuiNgocKhanh_LeQuangHuy/footerIndex.php
@@ -0,0 +1,43 @@
+<div class="container-fluid">	
+		<div class="row">
+			<hr class="light-100">
+			<div class="col-6">
+				<h5>&copy;  2019 - TRƯỜNG ĐẠI HỌC THỦY LỢI</h5>
+			</div>
+			<div class="col-6">
+					<a href="#"><i class="fab fa-facebook"></i></a>
+					<a href="#"><i class="fab fa-twitter"></i></a>
+					<a href="#"><i class="fab fa-youtube"></i></a>
+			</div>
+			<hr class="light-100">
+
+			<div class="col-md-4">
+				<div class="col-12">
+					
+					<iframe style="height: 250px; width: 450px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.6470214174765!2d105.82389757706271!3d21.00678184064981!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ac8109765ba5%3A0xd84740ece05680ee!2zVHLGsOG7nW5nIMSQ4bqhaSBI4buNYyBUaOG7p3kgTOG7o2k!5e0!3m2!1svi!2s!4v1573547250942!5m2!1svi!2s" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+				</div>
+			</div>
+			<div class="col-md-4">				
+				<p>TRƯỜNG ĐẠI HỌC THỦY LỢI - CƠ SỞ 2</p>
+				<p>Phường An Thạnh - TX Thuận An - Tỉnh Bình Dương</p>
+				<p>Điện thoại: (84).650.3748 620</p>
+				<p>Fax:(84).650.3833 489</p>
+				<hr class="light-100">
+				<p>VIỆN ĐÀO TẠO VÀ KHOA HỌC ỨNG DỤNG MIỀN TRUNG</p>
+				<p>Địa chỉ: Số 115 Trần Phú, Thành phố Phan Rang, Tỉnh Ninh Thuận</p>
+				<p>Điện thoại: 02593.823 027, 02593.823 028</p>
+				<p>Email: <a href="mailto:trungtamdh2@tlu.edu.vn?subject=feedback">trungtamdh2@tlu.edu.vn</a></p>
+			</div>
+			<div class="col-md-4">
+				<p>TRƯỜNG ĐẠI HỌC THỦY LỢI</p>
+				<p>Địa chỉ : 175 TÂY SƠN, ĐỐNG ĐA, HÀ NỘI.</p>
+				<p>Điện thoại: (024) 3852 2201 - Fax: (024) 3563 3351</p>
+				<p>Email: <a href="mailto:phonghcth@tlu.edu.vn?subject=feedback">phonghcth@tlu.edu.vn</a></p>
+				<hr class="light-100">
+				<p>TRƯỜNG ĐẠI HỌC THỦY LỢI - CƠ SỞ 2</p>
+				<p>Địa chỉ : Số 2 - Đường Trường Sa - P.17 - Q.Bình Thạnh - Tp.Hồ Chí Minh</p>
+				<p>Điện thoại: (84)028.38 400 532 - Fax: (84)028.38 400542</p>
+				<p>Email: <a href="mailto:cs2@tlu.edu.vn?subject=feedback">cs2@tlu.edu.vn</a></p>		
+			</div>
+		</div>
+	</div>
