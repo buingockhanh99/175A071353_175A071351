a2d8e06a1ee4050c93005c77ee96b0e193240301
ngockhanh99 <>
2019-11-23 14:26:55 +0700

    Update
    Thêm phần kết nối tới localhost và thêm jquery

---
+++ BTL_CNW_BuiNgocKhanh_LeQuangHuy/connect.php
@@ -0,0 +1,4 @@
+<?php 
+	$conn = mysqli_connect('localhost','root','','btl_cnw')or die('Kết nối thất bại!'.mysqli_connect_error());
+	mysqli_query($conn, 'SET NAMES UTF8');
+?>
--- BTL_CNW_BuiNgocKhanh_LeQuangHuy/index.php
+++ BTL_CNW_BuiNgocKhanh_LeQuangHuy/index.php
@@ -14,8 +14,12 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		
 		<!--css-->
-		<link rel="stylesheet" media="screen" type="text/css" href="css/index.css">
+		<link rel="stylesheet" media="screen" type="text/css" href="css/index123.css">
+		
 		<link rel="stylesheet" media="screen" type="text/css" href="css/bootstrap.min.css">
+		 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 		
 	</head>
 	<body>
