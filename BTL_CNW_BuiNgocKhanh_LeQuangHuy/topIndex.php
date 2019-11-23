0a8d76d6173d9e293cbcbc983c58226eaa316d46
ngockhanh99 <>
2019-11-23 09:55:18 +0700

    update
    Update phần Top

--- BTL_CNW_BuiNgocKhanh_LeQuangHuy/topIndex.php
+++ BTL_CNW_BuiNgocKhanh_LeQuangHuy/topIndex.php
@@ -41,7 +41,35 @@
             <li><a href="#" title="Đối ngoại">ĐỐI NGOẠI </a></li>
         </ul>             
     </div>
-      <img id="anh60nam" src="images/60nam.png">         
+   
+            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="0"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="0"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                    <img id="anh60nam"  src="images/60nam.png" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img id="anh60nam" src="images/2011.jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img id="anh60nam" src="images/chuoi.png" alt="First slide">
+                </div>
+            </div>
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span class="icon-prev" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <span class="icon-next" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
+      
 </div>
        
           
