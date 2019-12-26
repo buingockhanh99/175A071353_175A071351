14cb3730177de7c3c80b18406a9d03d75ce43806
ngockhanh99 <>
2019-12-26 07:31:42 +0700

    sửa lỗi giao diện

--- BTL_CNW_BuiNgocKhanh_LeQuangHuy/quanly/LopHocTheoNganhHoc.php
+++ BTL_CNW_BuiNgocKhanh_LeQuangHuy/quanly/LopHocTheoNganhHoc.php
@@ -16,28 +16,26 @@
 
     <div class="register-photo">
         <div class="form-container">
-            <div class="image-holder"></div>
+            <div class="image-holder">  
+            </div>
+                <?php 
+                    $sql = mysqli_query($conn,"select * from nganhhoc");          
+                ?>   
             <form method="post">
                 <h2 class="text-center">Tạo Lớp</h2>
-                <?php 
-                    $sql = mysqli_query($conn,"select * from nganhhoc") or die(myqli_error($conn));
-                    if (mysqli_num_rows($sql) > 0) {
-                    $i=0; 
-                ?>   
                 <div class="form-group">
                     <select class="form-control" name = "txtNganhhoc">
-                        <?php while($row=mysqli_fetch_assoc($sql)) {
-                        $i++; ?>
+                    <?php while($row=mysqli_fetch_assoc($sql)) {?>
                         <option><?php echo $row['NGANHHOC']; ?></option>
-                      <?php }}  ?>
+                    <?php } ?>
                     </select>
                 </div>
-
                 <div class="form-group"><input class="form-control" type="text" name="txtLophoc" placeholder="Lớp học"></div>
-                <div class="form-group"><button name="signup" class="btn btn-primary btn-block" type="submit">Tạo</button></div>
+                <div class="form-group"><button name="signup" class="btn btn-primary btn-block" type="submit">Tạo tài khoản</button></div>
             </form>
         </div>
     </div>
+
 
     <script src="../js/jquery.min.js"></script>
     <script src="../js/bootstrap.min.js"></script>
--- BTL_CNW_BuiNgocKhanh_LeQuangHuy/quanly/hocky-giaidoan.php
+++ BTL_CNW_BuiNgocKhanh_LeQuangHuy/quanly/hocky-giaidoan.php
@@ -4,9 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/dangky.css">
-    <link rel="stylesheet" href="css/quantri1.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/dangky.css">
     <title>Học kỳ - Giai đoạn</title>
 </head>
 
@@ -18,9 +17,8 @@
             </div>
             <form method="post">
                 <h2 class="text-center">Đăng ký</h2>
-                <div class="form-group"><input class="form-control" type="text" name="txtHocky" placeholder="Học Kỳ (VD: 2019 - 2020)"></div>
-                <div class="form-group"><input class="form-control" type="text" name="txtGiaidoan" placeholder="Giai Đoạn (VD: 1 -2)"></div>
-                <div class="form-group"><button name="signup" class="btn btn-primary btn-block" type="submit">Tạo tài khoản</button></div>
+                <div class="form-group"><input class="form-control" type="text" name="txtHocky" placeholder="Học Kỳ (VD: 2019-2020_1)"></div>
+                <div class="form-group"><button name="signup" class="btn btn-primary btn-block" type="submit">Tạo </button></div>
             </form>
         </div>
     </div>
@@ -34,27 +32,23 @@
     }
      
     //Nhúng file kết nối với database
-    include('connect.php');
+     require('../connect.php');
           
     //Khai báo utf-8 để hiển thị được tiếng việt
     header('Content-Type: text/html; charset=UTF-8');
-          
-
     $hocky      = $_POST['txtHocky'];
-    $giaidoan   = $_POST['txtGiaidoan'];
-
           
     //Kiểm tra người dùng đã nhập liệu đầy đủ chưa
-    if (!$hocky || !$giaidoan )
+    if (!$hocky)
     {
         echo  "<div style='text-align:center;color:#000000;'>Vui lòng nhập đầy đủ thông tin. <a href='javascript: history.go(-1)'>Trở lại</a></div>";
         exit;
     }
 
     //SELECT FROM hockygiaidoan WHERE HOCKY='$hocky' and GIAIDOAN ='$giaidoan'"
-     if (mysqli_num_rows(mysqli_query($conn,"SELECT * FROM hockygiaidoan WHERE HOCKY='$hocky' and GIAIDOAN ='$giaidoan'")) > 0)
+     if (mysqli_num_rows(mysqli_query($conn,"SELECT * FROM hockygiaidoan WHERE HOCKY='$hocky'")) > 0)
     {
-        echo "<div style='text-align:center;color:#000000;'>Đã có học kỳ và giai đoạn này <a href='javascript: history.go(-1)'>Trở lại</a></div>";
+        echo "<div style='text-align:center;color:#000000;'>Đã có học kỳ này <a href='javascript: history.go(-1)'>Trở lại</a></div>";
         exit;
     }  
 
@@ -63,7 +57,7 @@
     
     $add_hkgd = mysqli_query($conn, "
     INSERT INTO hockygiaidoan 
-    VALUE ('$hocky','$giaidoan')");
+    VALUE ('$hocky')");
     
                           
     //Thông báo quá trình lưu
